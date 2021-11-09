10934a3f38f6d2e3f38e271160adecac5e2a4973
Robin <roblan95@icloud.com>
2021-11-09 21:16:04 +0100

    dfdsf

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,4 @@
-import {
-    Routes,
-    Route
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Poll from "./components/poll";
 import Polls from "./components/polls";
@@ -10,14 +7,15 @@ import Signup from "./components/signup";
 import Login from "./components/login";
 
 const routes = (
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/poll/:id" element={<Poll />} />
-        <Route path="/polls" element={<Polls />} />
-        <Route path="/new" element={<NewPoll />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-    </Routes>
-)
+  //hej
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/poll/:id" element={<Poll />} />
+    <Route path="/polls" element={<Polls />} />
+    <Route path="/new" element={<NewPoll />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+);
 
 export default routes;
