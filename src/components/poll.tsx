a53b2bfc976a37fb22fa81255572640d2c97f7c1
Robin <roblan95@icloud.com>
2021-11-11 03:55:38 +0100

    fix bug

--- src/components/poll.tsx
+++ src/components/poll.tsx
@@ -1,18 +1,24 @@
-import { Box, Button, Heading, Radio, RadioGroup, Select, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Heading,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+} from "@chakra-ui/react";
 import { FunctionComponent, useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from 'react-router-dom';
-import { UserContext } from '../contextFile';
+import { useNavigate, useParams } from "react-router-dom";
+import { UserContext } from "../contextFile";
 
-interface PollProps {
-
-}
+interface PollProps {}
 
 export interface IPoll {
-    id: number;
-    options: string;
-    options_weight: number[];
-    question: string;
-    total_count: number;
+  id: number;
+  options: string;
+  options_weight: any;
+  question: string;
+  total_count: number;
 }
 
 /**
@@ -32,76 +38,102 @@ export interface IPoll {
  */
 
 const Poll: FunctionComponent<PollProps> = () => {
-    const { auth } = useContext(UserContext);
-    let { id } = useParams();
-    let navigate = useNavigate();
-    const [poll, setPoll] = useState<IPoll>();
-    const [loading, setLoading] = useState<boolean>(true);
-    const [voteSelected, setSelectedVote] = useState<number>();
+  const { auth } = useContext(UserContext);
+  let { id } = useParams();
+  let navigate = useNavigate();
+  const [poll, setPoll] = useState<IPoll>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [voteSelected, setSelectedVote] = useState<number>();
 
-    useEffect(() => {
-        if (!auth.user) {
-            navigate('/login')
-        }
-
-        (async () => {
-            const res = await fetch(`https://tva-backend.herokuapp.com/poll/${id}`);
-            const data = await res.json();
-            setPoll(data[0]);
-            setLoading(false);
-        })();
-    }, []);
-
-    const handleSelect = (e: string) => {
-        setSelectedVote(parseInt(e));
+  useEffect(() => {
+    if (!auth.user) {
+      navigate("/login");
     }
 
-    const handleSubmit = () => {
-        (async () => {
-            const res1 = await fetch(`https://tva-backend.herokuapp.com/poll/${id}/submit`, {
-                method: 'POST',
-                credentials: 'include',
-                body: JSON.stringify({
-                    choice: voteSelected,
-                    pollCount: poll?.total_count, 
-                    optionsWeight: poll?.options_weight 
-                })
-            });
-            const res2 = await fetch(`https://tva-backend.herokuapp.com/poll/${id}/submit-user`, {
-                method: 'POST',
-                credentials: 'include',
-                body: JSON.stringify({
-                    userId: auth.userId
-                })
-            });
-            const data1 = await res1.json();
-            const data2 = await res2.json();
+    (async () => {
+      const res = await fetch(`https://tva-backend.herokuapp.com/poll/${id}`);
+      const data = await res.json();
+      setPoll(data[0]);
+      setLoading(false);
+    })();
+  }, []);
 
-            console.log(data1, data2);
-        })();
-    }
+  const handleSelect = (e: string) => {
+    setSelectedVote(parseInt(e));
+  };
 
-    return (
-        <>
-            {loading ? "" :
-                <Box>
-                    <Heading as="h2" size="3xl">{poll!.question}</Heading>
-                    <RadioGroup onChange={(e)=>handleSelect(e)} value={voteSelected}>
-                        <Stack>
-                            {JSON.parse(poll!.options).map((option: string, index: string) => {
-                                return (
-                                    <Radio size="lg"colorScheme="red" value={index}>
-                                        {option}
-                                    </Radio>
-                                )
-                            })}
-                        </Stack>
-                    </RadioGroup>
-                    <Button onClick={handleSubmit} colorScheme="yellow">Vote</Button>
-                </Box>
-            }
-        </>
-    );
-}
+  const handleSubmit = () => {
+    (async () => {
+      if (poll) {
+        const optionsWeight = JSON.parse(poll.options_weight);
+        console.log(optionsWeight);
+
+        const bodyTest = {
+          choice: voteSelected,
+          pollCount: poll?.total_count,
+          optionsWeight,
+        };
+
+        console.log(bodyTest);
+        // const res1 = await fetch(`https://tva-backend.herokuapp.com/poll/${id}/submit`, {
+        const res1 = await fetch(`http://localhost:3001/poll/${id}/submit`, {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify(bodyTest),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const res2 = await fetch(
+          `https://tva-backend.herokuapp.com/poll/${id}/submit-user`,
+          {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify({
+              userId: auth.userId,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data1 = await res1.json();
+        const data2 = await res2.json();
+
+        console.log(data1, data2);
+      }
+    })();
+  };
+
+  return (
+    <>
+      {loading ? (
+        ""
+      ) : (
+        <Box>
+          <Heading as="h2" size="3xl">
+            {poll!.question}
+          </Heading>
+          <RadioGroup onChange={(e) => handleSelect(e)} value={voteSelected}>
+            <Stack>
+              {JSON.parse(poll!.options).map(
+                (option: string, index: string) => {
+                  return (
+                    <Radio size="lg" colorScheme="red" value={index}>
+                      {option}
+                    </Radio>
+                  );
+                }
+              )}
+            </Stack>
+          </RadioGroup>
+          <Button onClick={handleSubmit} colorScheme="yellow">
+            Vote
+          </Button>
+        </Box>
+      )}
+    </>
+  );
+};
 
 export default Poll;
